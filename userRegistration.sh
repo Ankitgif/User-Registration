764fc0b418f0b3fc1d0c5ec8bdd14b41f180755b
Ankitgif <ankitsinha2303@gmail.com>
2020-03-27 17:07:26 +0530

    [Ankit] Added to validate user last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -3,9 +3,19 @@ shopt -s extglob
 echo "Enter your first name : "
 read fname
 firstName="^[A-Z][a-z]{2,}$"
-if [[ $fname =~ $firstname ]]
+if [[ $fname =~ $firstName ]]
 then
 	echo "First Name is valid"
 else
 	echo "First Name is not valid"
 fi
+echo "Enter your Second name : "
+read sname
+secondName="^[A-Z][a-z]{2,}$"
+if [[ $sname =~ $secondName ]]
+then
+        echo "Second Name is valid"
+else
+        echo "Second Name is not valid"
+fi
+
